80ef2ebfdcc8e601cee1f3d075e2077e3314ff1c
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2026-04-04 21:12:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 92.35 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,34 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] operations) {
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
-        
-        for(String op : operations){
-            String[] s = op.split(" ");
-            String cmd = s[0];
-            int num = Integer.parseInt(s[1]);
-            
-            if(cmd.equals("I")){
-                mp.put(num, mp.getOrDefault(num, 0) + 1);
-            }
-            else{
-                if(mp.isEmpty()){
-                    continue;
-                }
-                int target = (num == 1) ? mp.lastKey() : mp.firstKey();
-                
-                if(mp.get(target)==1){
-                    mp.remove(target);
-                }
-                else{
-                    mp.put(target, mp.get(target) - 1);
-                }
-            }
-        }
-        if(mp.isEmpty()){
-            return new int[]{0,0};
-        }
-        return new int[]{mp.lastKey(), mp.firstKey()};
+        int[] answer = {};
+        return answer;
     }
 }
