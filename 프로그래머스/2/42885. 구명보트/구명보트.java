7915284c50de0fe0db9d85204cb364f3a4bf5ea9
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2026-04-04 20:13:22 +0900

    [level 2] Title: 구명보트, Time: 10.93 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,25 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
-        Arrays.sort(people);
-        
-        int l = 0; // 최소
-        int r = people.length - 1; // 최대
-        
-        while(l<=r){
-            if(people[l] + people[r] <= limit){
-                l++;
-                r--;
-            }
-            else{
-                r--;
-            }
-            answer++;
-        }
-        
         return answer;
     }
 }
