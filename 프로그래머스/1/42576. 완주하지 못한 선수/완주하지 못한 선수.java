aeb0b70526fe3683eac7ef65926243e49ff7a9ed
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2026-05-16 02:18:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.17 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<String, Integer> hm = new HashMap<>();
+        for(String player:participant){
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+        }
+        for(String player:completion){
+            hm.put(player, hm.get(player)-1);
+        }
+        
+        for(String key : hm.keySet()){
+            if(hm.get(key) != 0){
+                answer = key;
+                break;
+            }
+        }
+            
+        return answer;
+    }
+}
